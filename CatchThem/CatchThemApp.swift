b323c5495462f571528178ddf63029b34a57c9ba
keshew <artyom.korotckov2017@yandex.ru>
2025-09-01 12:47:03 +0500

    Initial Commit

---
+++ CatchThem/CatchThemApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchThemApp.swift
+//  CatchThem
+//
+//  Created by Артём Коротков on 01.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct CatchThemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
